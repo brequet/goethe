6517b4cd397ba8eea3a73a4ea5e31f2c4d7891fc
brequet <requet.baptiste@gmail.com>
2024-04-04 20:47:02 +0200

    front: added tailwind + shadcn

--- internal/webapp/frontend/src/lib/Counter.svelte
+++ internal/webapp/frontend/src/lib/Counter.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  let count: number = 0
+  import Button from "$lib/components/ui/button/button.svelte";
+
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
-<button on:click={increment}>
+<Button on:click={increment}>
   count is {count}
-</button>
+</Button>
